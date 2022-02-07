afb0528d2a26fcaa6aba437ae9235e03c2c421dd
Marc Malliate <marc.malliate@gmail.com>
2022-02-08 08:21:52 +1100

    reselvers updated and mutation added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,7 @@ const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).select("User");
+        return User.findOne({ _id: context.user._id }).select();
       }
       throw new AuthenticationError("You need to be logged in!");
     },
@@ -16,7 +16,9 @@ const resolvers = {
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
       if (!user) {
-        throw new AuthenticationError("No user found with this email address");
+        throw new AuthenticationError(
+          "Sorry, no user found with this email address"
+        );
       }
       const correctPw = await user.isCorrectPassword(password);
       if (!correctPw) {
@@ -30,19 +32,21 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    // Book is an input type
+
     saveBook: async (parent, { input }, context) => {
+      console.log("inside Save Book", context.user._id, input);
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
           { $addToSet: { savedBooks: input } },
           { new: true }
         );
+        console.log("updatedUser", updatedUser);
         return updatedUser;
       }
       throw new AuthenticationError("You need to be logged in!");
     },
-    // Returns all bookId and return User type.
+
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
