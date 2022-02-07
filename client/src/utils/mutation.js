c2b934849cd214884ed956bdc8cf25e6506acea0
Marc Malliate <marc.malliate@gmail.com>
2022-02-08 08:55:31 +1100

    mutation added

---
+++ client/src/utils/mutation.js
@@ -0,0 +1,67 @@
+import { gql } from "@apollo/client";
+
+//Login
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//Add user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+//Save book
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: SavedBookInput) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
+
+//Remove book
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
+    }
+  }
+`;
