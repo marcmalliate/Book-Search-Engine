314025f5e36879606476f07d0a26751077705406
Marc Malliate <marc.malliate@gmail.com>
2022-02-10 08:21:52 +1030

    config mongo updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/books", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Books", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
