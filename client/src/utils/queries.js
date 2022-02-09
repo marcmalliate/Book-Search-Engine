eb3a1e98752e63384f3f80e180297eb7d385791f
Marc Malliate <marc.malliate@gmail.com>
2022-02-10 07:06:29 +1030

    mutation added

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,11 +2,19 @@ import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
   query me {
-    User {
+    me {
+      _id
       username
       email
-      password
-      savedBooks
+      bookCount
+      savedBooks {
+        authors
+        description
+        bookId
+        image
+        link
+        title
+      }
     }
   }
 `;
